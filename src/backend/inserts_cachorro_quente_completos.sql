f9848731db5474a4894db9b19876f0dacebe81e6
Felipe Ferreira <felipe.fr.almeida@gmail.com>
2025-04-20 20:22:04 -0300

    fixing card food

---
+++ src/backend/inserts_cachorro_quente_completos.sql
@@ -0,0 +1,19 @@
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('SIMPLES SALADA', 14.00, 'Pão, Molho, Salsicha, Tomate, Alface e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('SIMPLES COM KIT', 14.00, 'Pão, Molho, Salsicha, Presunto, Queijo e Batata Palha', 'CACHORRO-QUENTE');
+
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('SIMPLES PIZZA', 14.00, 'Pão, Molho, Salsicha, Queijo, Tomate, Orégano e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('SIMPLES PIZZA E FRANGO', 16.00, 'Pão, Molho, Frango, Queijo, Tomate, Orégano e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('SIMPLES PIZZA E CATUPIRY', 16.00, 'Pão, Molho, Salsicha, Queijo, Catupiry, Tomate, Orégano e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('SIMPLES PIZZA FRANGO E CATUPIRY', 18.00, 'Pão, Molho, Frango, Queijo, Catupiry, Tomate, Orégano e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('SIMPLES PIZZA TUDO', 20.00, 'Pão, Molho, Salsicha, Frango, Presunto, Queijo, Catupiry, Tomate, Orégano e Batata Palha', 'CACHORRO-QUENTE');
+
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('SIMPLES COM QUEIJO, BACON E CATUPIRY', 18.00, 'Pão, Molho, Salsicha, Queijo, Bacon, Catupiry e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('SIMPLES COM FRANGO E QUEIJO', 16.00, 'Pão, Molho, Frango, Queijo e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('SIMPLES COM FRANGO, QUEIJO E BACON', 18.00, 'Pão, Molho, Frango, Queijo, Bacon e Batata Palha', 'CACHORRO-QUENTE');
+
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('HOT QUEIJO', 18.00, 'Pão, Molho, Salsicha, Tomate, Alface, Queijo, Bacon, Maionese, Catchup, Mostarda e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('TIM DO LELE', 16.00, 'Pão, Molho, Salsicha, Tomate, Alface, Milho, Catupiry, Maionese, Catchup, Mostarda e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('HOT FRANGO', 16.00, 'Pão, Molho, Frango, Milho, Alface, Catchup, Maionese e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('HOT FRANGO QUEIJO', 18.00, 'Pão, Molho, Frango, Queijo, Alface, Tomate, Maionese e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('HOT FRANGO BACON', 18.00, 'Pão, Molho, Frango, Queijo, Bacon, Alface, Tomate, Maionese e Batata Palha', 'CACHORRO-QUENTE');
+INSERT INTO LANCHES (NOME, PRECO, INGREDIENTES, TIPO) VALUES ('HOT FRANGO MIX', 20.00, 'Pão, Molho, Frango, Queijo, Bacon, Alface, Tomate, Maionese e Batata Palha', 'CACHORRO-QUENTE');
