79e9ec4721359458bf1ec5d4c39ee7f516b320d6
fabin99 <fabiopeixoto213@gmail.com>
2025-05-21 17:27:54 -0300

    Codigo duplicado

--- src/frontend/tim-do-lele/src/app/components/navbar/navbar.component.ts
+++ src/frontend/tim-do-lele/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, ViewChild, OnInit, HostListener } from '@angular/core';
-import { Component, ElementRef, ViewChild, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartService } from 'app/service/cart.service';
 import { Food } from 'app/models/Food.model';
