0c50c760d08315f19536a84fe9a23e6d1da25dfb
Felipe Ferreira <felipe.fr.almeida@gmail.com>
2025-05-26 17:48:55 -0300

    click whatsapp

--- src/frontend/tim-do-lele/src/app/components/rodape/rodape.component.ts
+++ src/frontend/tim-do-lele/src/app/components/rodape/rodape.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './rodape.component.scss'
 })
 export class RodapeComponent {
+  openWhatsapp() {
+  window.open(
+    'https://wa.me/5516992241633?text=Olá!%20Gostaria%20de%20fazer%20um%20pedido',
+    '_blank'
+  );
+}
 
 }
